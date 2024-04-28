60a42d453116fa5c658166f19388f1d34aafe5a7
Dubjay18 <jejeniyi7@gmail.com>
2024-04-28 18:54:15 +0100

    first commit

---
+++ app/app.go
@@ -0,0 +1,9 @@
+package app
+
+func StartServer() {
+	// Start the server
+
+	// This is where the server would be started
+	// but for now we will just print a message
+	println("Welcome to sanctum")
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Dubjay18/sanctum-server/app"
+
+func main() {
+	// Start the server
+	app.StartServer()
+}
