6b1e412a30eda6dae8f08ce81dd584369c0c1cf0
Dubjay18 <jejeniyi7@gmail.com>
2024-05-18 19:22:12 +0100

    chore:wipe

--- app/app.go
+++ app/app.go
@@ -1,12 +1,8 @@
 package app
 
 import (
-	"fmt"
-	"github.com/Dubjay18/sanctum-server/app/controller"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	socketio "github.com/googollee/go-socket.io"
-	log "github.com/sirupsen/logrus"
 	"time"
 )
 
@@ -14,9 +10,6 @@ func StartServer() {
 	//hub := dao.NewHub()
 	//wsHandler := controller.NewWsController(hub)
 	//go hub.Run()
-
-	server := socketio.NewServer(nil)
-	socketHandler := controller.NewSocketController()
 
 	router := gin.Default()
 	router.Use(gin.Logger())
@@ -40,30 +33,5 @@ func StartServer() {
 		})
 	})
 
-	server.OnConnect(controller.SocketConnection, socketHandler.HandleConnection)
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		log.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-
-	server.OnError("/", func(s socketio.Conn, e error) {
-		// server.Remove(s.ID())
-		fmt.Println("meet error:", e)
-	})
-
-	server.OnDisconnect("/", func(s socketio.Conn, reason string) {
-		// Add the Remove session id. Fixed the connection & mem leak
-		//server.Remove(s.ID())
-		fmt.Println("closed", reason)
-	})
-
-	go server.Serve()
-	defer server.Close()
-	//router.POST("/create-room", wsHandler.CreateRoom)
-	//router.GET("/join-room/:id/:user_id/:username", wsHandler.JoinRoom)
-	//router.GET("/get-rooms", wsHandler.GetRooms)
-	//router.GET("/get-clients/:id", wsHandler.GetClients)
-	router.GET("/socket.io/*any", gin.WrapH(server))
-	router.POST("/socket.io/*any", gin.WrapH(server))
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
