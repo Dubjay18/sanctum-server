d303841e5487d02eb6dd26db862ac1b94ea83d3a
Dubjay18 <jejeniyi7@gmail.com>
2024-05-20 18:54:35 +0100

    fix

--- app/domain/dao/client.go
+++ app/domain/dao/client.go
@@ -1,1 +1,11 @@
 package dao
+
+import "github.com/gorilla/websocket"
+
+type Client struct {
+	hub *Hub
+	// The websocket connection.
+	conn *websocket.Conn
+	// Buffered channel of outbound messages.
+	send chan []byte
+}
--- app/domain/dao/hub.go
+++ app/domain/dao/hub.go
@@ -1,1 +1,12 @@
 package dao
+
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+	// Inbound messages from the clients.
+	broadcast chan []byte
+	// Register requests from the clients.
+	register chan *Client
+	// Unregister requests from clients.
+	unregister chan *Client
+}
