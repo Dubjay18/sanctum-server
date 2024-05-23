f95aed4b6e5dd7fa9f15e511321a5f76f4991e10
Dubjay18 <jejeniyi7@gmail.com>
2024-05-23 11:06:18 +0100

    feat: hub methods

--- app/domain/dao/hub.go
+++ app/domain/dao/hub.go
@@ -25,3 +25,40 @@ func NewHub() *Hub {
 		Broadcast:  make(chan *Message, 5),
 	}
 }
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case cl := <-h.Register:
+			if _, ok := h.Rooms[cl.RoomId]; ok {
+				r := h.Rooms[cl.RoomId]
+				if _, ok := r.Clients[cl.ID]; !ok {
+					r.Clients[cl.ID] = cl
+				}
+			}
+		case cl := <-h.Unregister:
+			if _, ok := h.Rooms[cl.RoomId]; ok {
+				r := h.Rooms[cl.RoomId]
+				if _, ok := r.Clients[cl.ID]; ok {
+					if len(r.Clients) != 0 {
+						h.Broadcast <- &Message{
+							Content:  cl.Username + " has left the room",
+							RoomId:   cl.RoomId,
+							Username: cl.Username,
+						}
+					}
+					delete(r.Clients, cl.ID)
+					close(cl.Message)
+				}
+			}
+		case msg := <-h.Broadcast:
+			if _, ok := h.Rooms[msg.RoomId]; ok {
+				r := h.Rooms[msg.RoomId]
+				for _, cl := range r.Clients {
+					cl.Message <- msg
+				}
+			}
+
+		}
+	}
+}
